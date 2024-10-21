9cbf3c9e76c7f309ddcc189d5b772d3e83d7ff6b
lauraferreras <laura.ferreras01@estudiant.upf.edu>
2024-10-21 18:18:17 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,34 @@
-## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
-## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
-## COMMAND LINE SPECIFIED BELOW
 def main(x) :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+    ## YOU CODE SHOULD START HERE AST THE SAME
+    ## IDENTATION AS THIS COMMENT
+    c = 1
+    z = 0
 
+    while c <= x :
+        if (x % c == 0) :
+            z = z+1
+        c = c+1
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+    x = x-1
+    y = 0
+    while x >= 1 and y < z :
+        c = 1
+        y = 0
+        while c <= x :
+            if (x % c == 0) :
+                y = y+1
+            c = c+1
+        x = x-1
+
+    if y >= z :
+        return("not anti-prime")
+    else :
+        return("anti-prime")
+
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+    import sys
+    x = int(sys.argv[1])
+    print(main(x))
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
